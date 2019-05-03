c04399d26386f46c546952417f70b73528675219
geraldopessoajr <44471626+geraldopessoajr@users.noreply.github.com>
2019-05-03 08:45:02 -0300

    Update coautoriatodos1002.js

--- js/coautoriatodos1002.js
+++ js/coautoriatodos1002.js
@@ -82,13 +82,13 @@ chart.padding(30, 30, 10, 0);
 chart.legend = new am4charts.Legend();
 chart.legend.itemContainers.template.cursorOverStyle = am4core.MouseCursorStyle.pointer;
 
-var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
 categoryAxis.dataFields.category = "category";
 categoryAxis.renderer.minGridDistance = 60;
 categoryAxis.renderer.grid.template.location = 0;
 categoryAxis.interactionsEnabled = false;
 
-var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
+var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 valueAxis.min = 0;
 valueAxis.max = 100;
 valueAxis.strictMinMax = true;
@@ -98,13 +98,13 @@ valueAxis.renderer.minGridDistance = 20;
 valueAxis.renderer.minWidth = 35;
 
 var series1 = chart.series.push(new am4charts.ColumnSeries());
-series1.columns.template.tooltipText = "{name}: {valueX.totalPercent.formatNumber('#.0')}%";
+series1.columns.template.tooltipText = "{name}: {valueY.totalPercent.formatNumber('#.0')}%";
 series1.columns.template.column.strokeOpacity = 1;
 series1.name = "Humanities";
-series1.dataFields.categoryY = "category";
-series1.dataFields.valueX = "value7";
-series1.dataFields.valueXShow = "totalPercent";
-series1.dataItems.template.locations.categoryY = 0.5;
+series1.dataFields.categoryX = "category";
+series1.dataFields.valueY = "value7";
+series1.dataFields.valueYShow = "totalPercent";
+series1.dataItems.template.locations.categoryX = 0.5;
 series1.stacked = true;
 series1.tooltip.pointerOrientation = "vertical";
 series1.tooltip.dy = - 20;
@@ -112,58 +112,58 @@ series1.fill = "#74BF00";
 series1.cursorHoverEnabled = false;
 
 var bullet1 = series1.bullets.push(new am4charts.LabelBullet());
-bullet1.label.text = "{valueX.totalPercent.formatNumber('#.0')}";
+bullet1.label.text = "{valueY.totalPercent.formatNumber('#.0')}";
 bullet1.fontSize = 12;
-bullet1.locationX = 0.5;
+bullet1.locationY = 0.5;
 bullet1.label.truncate = false;
 bullet1.label.fill = am4core.color("#ffffff");
 bullet1.interactionsEnabled = false;
 
 var series4 = chart.series.push(series1.clone());
 series4.name = "Health Sciences";
-series4.dataFields.valueX = "value6";
+series4.dataFields.valueY = "value6";
 series4.fill = "#DD87FB";
 series4.stroke = series4.fill;
 series4.cursorHoverEnabled = false;
 
 var series3 = chart.series.push(series1.clone());
 series3.name = "Linguistics, Letters and Arts";
-series3.dataFields.valueX = "value8";
+series3.dataFields.valueY = "value8";
 series3.fill = "#D1B7A9";
 series3.stroke = series3.fill;
 series3.cursorHoverEnabled = false;
 
 var series5 = chart.series.push(series1.clone());
 series5.name = "Biological Sciences         ";
-series5.dataFields.valueX = "value3";
+series5.dataFields.valueY = "value3";
 series5.fill = "#4D5048";
 series5.stroke = series5.fill;
 series5.cursorHoverEnabled = false;
 
 var series8 = chart.series.push(series1.clone());
 series8.name = "Engineering";
-series8.dataFields.valueX = "value4";
+series8.dataFields.valueY = "value4";
 series8.fill = "#3BBE90";
 series8.stroke = series8.fill;
 series8.cursorHoverEnabled = false;
 
 var series6 = chart.series.push(series1.clone());
 series6.name = "Applied and Social Sciences";
-series6.dataFields.valueX = "value2";
+series6.dataFields.valueY = "value2";
 series6.fill = "#F88B16";
 series6.stroke = series6.fill;
 series6.cursorHoverEnabled = false;
 
 var series2 = chart.series.push(series1.clone());
 series2.name = "Agrarian Sciences";
-series2.dataFields.valueX = "value1";
+series2.dataFields.valueY = "value1";
 series2.fill = "#F65582";
 series2.stroke = series2.fill;
 series2.cursorHoverEnabled = false;
 
 var series7 = chart.series.push(series1.clone());
 series7.name = "Exact and Earth Sciences";
-series7.dataFields.valueX = "value5";
+series7.dataFields.valueY = "value5";
 series7.fill = "#3FC0FF";
 series7.stroke = series7.fill;
 series7.cursorHoverEnabled = false;
